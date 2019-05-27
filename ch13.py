edce5b0e71f2fee016a0a59220aebe533edc6f2a
ProDesk <fob330@gmail.com>
2019-05-27 17:10:58 +0900

    commit

---
+++ ch13.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+plt.rcParams['font.family'] = 'NanumBarunGothic'
+plt.rcParams['axes.unicode_minus'] = False
+
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "cnn"
+
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
