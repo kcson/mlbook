515af4457c9aa3558b61b70e0f2c36770890099c
kcson <kcson@fasoo.comm>
2019-07-05 01:13:16 +0900

    commit

--- ch13.py
+++ ch13.py
@@ -27,3 +27,15 @@ def save_fig(fig_id, tight_layout=True):
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format='png', dpi=300)
+
+
+def plot_image(image):
+    plt.imshow(image, cmap="gray", interpolation="nearest")
+    plt.axis("off")
+
+
+def plot_color_image(image):
+    plt.imshow(image.astype(np.uint8), interpolation="nearest")
+    plt.axis("off")
+
+
