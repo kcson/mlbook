241c290f9000e118b85caab40bc1b2ab2182381c
kcson <kcson@fasoo.comm>
2019-07-15 01:08:18 +0900

    commit

--- ch13.py
+++ ch13.py
@@ -44,9 +44,28 @@ china = load_sample_image("china.jpg")
 flower = load_sample_image("flower.jpg")
 dataset = np.array([china, flower], dtype=np.float32)
 batch_size, height, width, channels = dataset.shape
+print(china.shape)
+print(dataset.shape)
 
 filters = np.zeros(shape=(7, 7, channels, 2), dtype=np.float32)
 # filter
-filters[:, 3, :, 0] = 1 #수직
-filters[3, :, :, 1] = 1 #수평
+filters[:, 3, :, 0] = 1  # 수직
+filters[3, :, :, 1] = 1  # 수평
 
+X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+# convolution = tf.nn.conv2d(X, filters, strides=[1, 2, 2, 1], padding="SAME")
+convolution = tf.layers.conv2d(X, filters=2, kernel_size=7, strides=[2, 2], padding="SAME")
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    init.run()
+    output = sess.run(convolution, feed_dict={X: dataset})
+
+# plt.imshow(output[1, :, :, 1], cmap="gray")
+# plt.show()
+
+for image_index in (0, 1):
+    for feature_map_index in (0, 1):
+        plt.imshow(output[image_index, :, :, feature_map_index], cmap="gray")
+        plt.show()
